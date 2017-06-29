317032b24223fc80a012e540ecb367ccd346bc9c
mozokevgen <mozokevgen@gmail.com>
2017-06-29 12:08:02 +0300

    Add work in minimize mode need to be tested and simplyfied

--- SimpleDoor/MainPage.xaml.cs
+++ SimpleDoor/MainPage.xaml.cs
@@ -4,10 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
+using Windows.ApplicationModel.ExtendedExecution;
 using Windows.Devices.Enumeration;
 using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
@@ -31,6 +33,15 @@ namespace SimpleDoor
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        // A pointer back to the main page.  This is needed if you want to call methods in MainPage such
+        // as NotifyUser()
+        //private MainPage rootPage = MainPage.Current;
+        public static MainPage Current;
+
+        private ExtendedExecutionSession session = null;
+        private Timer periodicTimer = null;
+
         public MqttClient client;
         public CancellationTokenSource ReadCancellationTokenSource;
         private SerialDevice serialPort = null;
@@ -181,7 +192,7 @@ namespace SimpleDoor
                 if ((fstLetter >= 0) && (lstLetter > 0)) strFromPort = strFromPort.Substring(fstLetter, lstLetter - fstLetter);
 
                 this.client.Publish("AS/DoorCoworkingOut/cardID", Encoding.UTF8.GetBytes(strFromPort));
-                SerialPoprtData.Text = strFromPort;
+                SerialPoprtData.Text = Regex.Replace(strFromPort, @"\t|\n|\r", "");
 
                 //StatusTextBlock.Text = strFromPort;
 
@@ -240,11 +251,92 @@ namespace SimpleDoor
         {
             MqttConnect();
             SerialConnection();
+            ClearExtendedExecution();
+            BeginExtendedExecution();
         }
 
         private void Page_Unloaded(object sender, RoutedEventArgs e)
         {
             CancelReadTask();
+            EndExtendedExecution();
+        }
+
+        private async void BeginExtendedExecution()
+        {
+            // The previous Extended Execution must be closed before a new one can be requested.
+            // This code is redundant here because the sample doesn't allow a new extended
+            // execution to begin until the previous one ends, but we leave it here for illustration.
+            ClearExtendedExecution();
+
+            var newSession = new ExtendedExecutionSession();
+            newSession.Reason = ExtendedExecutionReason.Unspecified;
+            newSession.Description = "Raising periodic toasts";
+            newSession.Revoked += SessionRevoked;
+            ExtendedExecutionResult result = await newSession.RequestExtensionAsync();
+
+            switch (result)
+            {
+                case ExtendedExecutionResult.Allowed:
+                    //Current.NotifyUser("Extended execution allowed.", NotifyType.StatusMessage);
+                    session = newSession;
+                    periodicTimer = new Timer(OnTimer, DateTime.Now, TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(10));
+                    break;
+
+                default:
+                case ExtendedExecutionResult.Denied:
+                    //rootPage.NotifyUser("Extended execution denied.", NotifyType.ErrorMessage);
+                    newSession.Dispose();
+                    break;
+            }
+            //UpdateUI();
+        }
+
+        private void OnTimer(object state)
+        {
+            var startTime = (DateTime)state;
+            var runningTime = Math.Round((DateTime.Now - startTime).TotalSeconds, 0);
+            //MainPage.DisplayToast($"Extended execution has been active for {runningTime} seconds");
+        }
+
+        private async void SessionRevoked(object sender, ExtendedExecutionRevokedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                switch (args.Reason)
+                {
+                    case ExtendedExecutionRevokedReason.Resumed:
+                        //rootPage.NotifyUser("Extended execution revoked due to returning to foreground.", NotifyType.StatusMessage);
+                        break;
+
+                    case ExtendedExecutionRevokedReason.SystemPolicy:
+                        //rootPage.NotifyUser("Extended execution revoked due to system policy.", NotifyType.StatusMessage);
+                        break;
+                }
+
+                EndExtendedExecution();
+            });
+        }
+
+        void ClearExtendedExecution()
+        {
+            if (session != null)
+            {
+                session.Revoked -= SessionRevoked;
+                session.Dispose();
+                session = null;
+            }
+
+            if (periodicTimer != null)
+            {
+                periodicTimer.Dispose();
+                periodicTimer = null;
+            }
+        }
+
+        private void EndExtendedExecution()
+        {
+            ClearExtendedExecution();
+            
         }
     }
 }
